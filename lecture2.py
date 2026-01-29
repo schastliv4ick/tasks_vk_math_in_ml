92f95ba99ec7975e4dfb56b3d84c2074bfefc71d
Maria Koltakova <kol06m@yandex.ru>
2026-01-29 19:38:14 +0000

    finil task1 lection2

--- lecture2.py
+++ lecture2.py
@@ -4,9 +4,23 @@ def rec(a, Dig, deep):
             a.append(i)
             rec(a, Dig, deep)
         if deep-1  == len(a):
-            print("".join(map(str, a))+str(i))
+            if len(a) != 0:
+                print(" ".join(map(str, a)), i)
+            else:
+                print(i)
     if len(a) > 0:
         a.pop()
+
+
+
+
+def sist(a, st):
+    s = ''
+    while a != 0:
+        s = str(a % st) + s
+        a = a // st
+    s = '0'*(st - len(s))+s
+    return s
 
 
 
@@ -15,3 +29,6 @@ N, maxDig = map(int, input().split()) # размер размещения и м�
 cnt = (maxDig+1)**N
 a = []
 rec(a, maxDig, N)
+
+#for i in range((maxDig+1)**(N)):
+#    print(sist(i, N))
