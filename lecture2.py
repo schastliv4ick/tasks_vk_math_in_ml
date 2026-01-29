455afff2b01deca6c73d4d60e82df275533d1d48
Maria Koltakova <kol06m@yandex.ru>
2026-01-29 18:59:48 +0000

    working version

--- lecture2.py
+++ lecture2.py
@@ -1,19 +1,17 @@
 def rec(a, Dig, deep):
     for i in range(Dig + 1):
-        if len(a) < deep:
+        if len(a) < deep - 1:
             a.append(i)
             rec(a, Dig, deep)
-        if deep == len:
-            for x in a:
-                print(x)
-            print(i, '\n')
+        if deep-1  == len(a):
+            print("".join(map(str, a))+str(i))
     if len(a) > 0:
         a.pop()
 
 
 
-N = int(input()) # размер размещения
-maxDig = int(input()) # максимальная цифра
+N, maxDig = map(int, input().split()) # размер размещения и максимальная цифра
+
 cnt = (maxDig+1)**N
 a = []
 rec(a, maxDig, N)
